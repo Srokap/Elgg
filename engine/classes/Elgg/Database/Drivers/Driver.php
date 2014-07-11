5f5cacae0ae0cca4b91bcd81e76d014456aae04d
Paweł Sroka <srokap@gmail.com>
2014-07-11 03:10:22 +0200

    feature(dbal): Basic driver for DB abstraction WIP

---
+++ engine/classes/Elgg/Database/Drivers/Driver.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * WARNING: API IN FLUX. DO NOT RELY ON IT OUTSIDE OF THE CORE.
+ *
+ * Inteface defining essential functions required from every DB type to work with Elgg.
+ */
+interface Driver {
+	/*
+	 * Mysql calls used
+	 *
+	 * mysql_connect
+	 * mysql_select_db
+	 * mysql_query
+	 * mysql_fetch_object
+	 * mysql_fetch_array
+	 * mysql_fetch_assoc
+	 * mysql_free_result
+	 * mysql_close
+	 * mysql_insert_id
+	 * mysql_affected_rows
+	 * mysql_num_rows
+	 * mysql_errno
+	 * mysql_real_escape_string
+	 * mysql_get_server_info
+	 */
+}
